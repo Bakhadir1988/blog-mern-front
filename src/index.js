d98e3cda2f50b91dc451263479287a8d51ecc8ea
Bakhadir1988 <myblogfree1988@gmail.com>
2023-01-24 00:33:42 +0300

    В работе скилетон

--- src/index.js
+++ src/index.js
@@ -4,20 +4,25 @@ import App from "./App";
 import CssBaseline from "@mui/material/CssBaseline";
 
 import { BrowserRouter } from "react-router-dom";
-
-import "./index.scss";
 import { ThemeProvider } from "@mui/material";
 import { theme } from "./theme";
+import { Provider } from "react-redux";
+
+import store from "./redux/store";
+
+import "./index.scss";
 
 const root = ReactDOM.createRoot(document.getElementById("root"));
 
 root.render(
-  <React.StrictMode>
+  <>
     <CssBaseline />
     <ThemeProvider theme={theme}>
       <BrowserRouter>
-        <App />
+        <Provider store={store}>
+          <App />
+        </Provider>
       </BrowserRouter>
     </ThemeProvider>
-  </React.StrictMode>
+  </>
 );
